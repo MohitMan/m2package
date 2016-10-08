24445704ebd0f57574ab30283badb922b7c36b46
MohitMan <mohitman1989@gmail.com>
2016-10-08 18:55:32 +0530

    tag ing

--- src/test/features/m2feature.feature
+++ src/test/features/m2feature.feature
@@ -1,6 +1,13 @@
 Feature: This is a sample google coordinate BDD implementation 
 
+@TEST
 Scenario: Check google coordinate 
+	Given URL details for google 
+	When I search anything 
+	Then I should see page coordinate
+	
+@DEV
+Scenario: Check google coordinate MO
 	Given URL details for google 
 	When I search anything 
 	Then I should see page coordinate
--- target/cucumber-htmlreport/report.js
+++ target/cucumber-htmlreport/report.js
@@ -7,29 +7,35 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 8654652015,
+  "duration": 9083676047,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 3,
+  "line": 4,
   "name": "Check google coordinate",
   "description": "",
   "id": "this-is-a-sample-google-coordinate-bdd-implementation;check-google-coordinate",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@TEST"
+    }
+  ]
 });
 formatter.step({
-  "line": 4,
+  "line": 5,
   "name": "URL details for google",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 5,
+  "line": 6,
   "name": "I search anything",
   "keyword": "When "
 });
 formatter.step({
-  "line": 6,
+  "line": 7,
   "name": "I should see page coordinate",
   "keyword": "Then "
 });
@@ -37,25 +43,83 @@ formatter.match({
   "location": "M2class.url_details_for_google()"
 });
 formatter.result({
-  "duration": 8472395560,
+  "duration": 9048308959,
   "status": "passed"
 });
 formatter.match({
   "location": "M2class.i_search_anything()"
 });
 formatter.result({
-  "duration": 831749985,
+  "duration": 844309857,
   "status": "passed"
 });
 formatter.match({
   "location": "M2class.i_should_see_page_coordinate()"
 });
 formatter.result({
-  "duration": 294907223,
+  "duration": 191047719,
   "status": "passed"
 });
 formatter.after({
-  "duration": 2606690812,
+  "duration": 2351041038,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 5743374687,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Check google coordinate MO",
+  "description": "",
+  "id": "this-is-a-sample-google-coordinate-bdd-implementation;check-google-coordinate-mo",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 9,
+      "name": "@DEV"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "URL details for google",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "I search anything",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "I should see page coordinate",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "M2class.url_details_for_google()"
+});
+formatter.result({
+  "duration": 8989055246,
+  "status": "passed"
+});
+formatter.match({
+  "location": "M2class.i_search_anything()"
+});
+formatter.result({
+  "duration": 763248818,
+  "status": "passed"
+});
+formatter.match({
+  "location": "M2class.i_should_see_page_coordinate()"
+});
+formatter.result({
+  "duration": 196762840,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1668785534,
   "status": "passed"
 });
 });
